a7edfa776f13cc8ca498d8edf3257be88b95bed9
NauyHL <liuhy.df.xin@gmail.com>
2022-08-08 13:44:40 +0900

    args

--- args.py
+++ args.py
@@ -3,12 +3,12 @@ import yaml
 
 def get_train_args_parser(add_help=True):
     parser = argparse.ArgumentParser(description='Default odcore args', add_help=add_help)
-    parser.add_argument('--resume-exp', default='running_log/yolo_ch', type=str, help='resume training from exp path')
+    parser.add_argument('--resume-exp', default='', type=str, help='resume training from exp path')
     parser.add_argument('--conf-file', default='', type=str, help='config file path')
     parser.add_argument('--ckpt-file', default='', type=str, help='load the ckpt file which is compatible with config ')
     parser.add_argument('--fine-tune', default='', type=str, help='load the ckpt file only for model loading')
     parser.add_argument('--batch-size', default=32, type=int, help='number of batchsize')
-    parser.add_argument('--workers', default=1, type=int, help='number of data loading workers (default: 8)')
+    parser.add_argument('--workers', default=8, type=int, help='number of data loading workers (default: 8)')
     parser.add_argument('--device', default='0', type=str, help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--eval-interval', default=20, type=int, help='evaluate at every interval epochs')
     return parser
