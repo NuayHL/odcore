65d390fd7584317e2de93df05498807d20c438d6
NauyHL <liuhaoyuan@akane.waseda.jp>
2022-08-14 17:06:41 +0900

    add eval

--- data/dataloader.py
+++ data/dataloader.py
@@ -8,10 +8,11 @@ def build_dataloader(anns_path, img_path, config_data, batch_size, rank, workers
     workers = min(os.cpu_count() // int(os.getenv("WORLD_SIZE", 1)),
                   batch_size if batch_size > 1 else 0,
                   workers)
-    is_persistent = True if workers > 1 else False
+    is_persistent = True if workers > 1 and task =='train' else False
+    is_shuffle = False if task == 'val' else True
     loader = DataLoader(dataset,
                         batch_size=batch_size,
-                        shuffle=True,
+                        shuffle=is_shuffle,
                         num_workers=workers,
                         persistent_workers=is_persistent,
                         collate_fn=dataset.OD_default_collater,
