82e3e21d95c490642202b14d874c45fcb7ade88a
NuayHL <liuhaoyuan@akane.waseda.jp>
2022-08-15 00:11:49 +0900

    fine eval

--- engine/eval.py
+++ engine/eval.py
@@ -8,6 +8,7 @@ from pycocotools.cocoeval import COCOeval
 from pycocotools.coco import COCO
 from data.dataloader import build_dataloader
 from data.data_augment import Normalizer
+from data.result import Result
 from utils.misc import progressbar
 
 class Eval():
